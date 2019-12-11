02a30f53a0c057d9bfd475b4d482c6d231eed8b3
Adam Garling <adamgarling@Adams-MacBook-Pro.local>
2019-12-10 20:25:16 -0800

    created user places page and components

---
+++ src/places/pages/UserPlaces.js
@@ -0,0 +1,43 @@
+import React from 'react'
+import { useParams } from 'react-router-dom';
+
+import PlaceList from '../components/PlaceList';
+
+const DUMMY_PLACES = [
+    {
+        id: 'p1',
+        title: 'Empire State Building',
+        description: 'One of the tallest buildings in the world',
+        imageUrl: 'https://via.placeholder.com/500',
+        address: '20 W 34th St, New York, NY 1001',
+        loaction: {
+            lat: 40.7484405,
+            long: -73.98785884,
+        },
+        creator: 'u1'
+    },
+    {
+        id: 'p2',
+        title: 'Empire State Building Clone',
+        description: 'One of the tallest buildings in the world',
+        imageUrl: 'https://via.placeholder.com/500',
+        address: '20 W 34th St, New York, NY 1001',
+        loaction: {
+            lat: 40.7484405,
+            long: -73.98785884,
+        },
+        creator: 'u2'
+    }
+]
+
+const UserPlaces = () => {
+    const userId = useParams().userId;
+    console.log(userId)
+    const loadedPlaces = DUMMY_PLACES.filter(place => place.creator === userId);
+    return <PlaceList items={loadedPlaces}/>
+}
+
+export default UserPlaces;
+
+
+
